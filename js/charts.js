6a88e0dd551da2ea4ba80e8a80e42f54e338d2c3
CarlS2rt <94241665+CarlS2rt@users.noreply.github.com>
2022-03-12 22:39:52 -0600

    bar graph

--- js/charts.js
+++ js/charts.js
@@ -58,30 +58,38 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-    var samples = data.
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
+    var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
 
     //  5. Create a variable that holds the first sample in the array.
-
+    var sampleResult = sampleArray[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var sampleIds =  sampleArray.otu_ids;
+    var sampleLabel = sampleArray.otu_labels;
+    var sampleValue = sampleArray.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    var yticks = sampleIds.slice(0,10).map(otu => otu.sampleIds).reverse();
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      type: "bar",
+      orientation: 'h',
+      text: sampleLabel,
+      x: sampleValue,
+      y: yticks, 
+    }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      name: "Top 10 Belly Button Cultures Found",
+      
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot('bar', barData, barLayout);
   });
 }
